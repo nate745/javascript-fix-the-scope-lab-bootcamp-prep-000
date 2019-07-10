49b8eaae3652e5a2d9617916253f1c3c90a75e7d
Nathan Alex <nate745@gmail.com>
2019-07-10 07:43:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,8 +27,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 
-var theFunk() = function(){
-  return function(){
-
-  }
-}
+return theFunk
